72d884ccc4979653b33c47679f4242c7b15d4808
Kevin Koech <kevinkoech@proton.me>
2024-01-12 23:08:42 +0300

    add all functions needed for simple calculations

---
+++ calculator/operands.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+Contains all defined operands for the simple calculator.
+"""
+
+
+def add(arg1, arg2):
+    """
+    Perform addition.
+    """
+    return arg1 + arg2
+
+
+def subtract(arg1, arg2):
+    """
+    Perform subtraction.
+    """
+    return arg1 - arg2
+
+
+def multiply(arg1, arg2):
+    """
+    Perform multiplication.
+    """
+    return arg1 * arg2
+
+
+def divide(arg1, arg2):
+    """
+    Perform division.
+    """
+    return arg1 / arg2
+
+
+def find_remainder(arg1, arg2):
+    """
+    Find remainder.
+    """
+    return arg1 % arg2
