68d37cd1f1fd241b099b05238a82c067a9ec7358
Kevin Koech <kevinkoech@proton.me>
2024-02-21 14:10:04 +0300

    perform migrations and fix session errors

--- contact_book/app/__init__.py
+++ contact_book/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-from flask_session import Session
+from flask_session import Session, SqlAlchemySessionInterface
 from flask_migrate import Migrate
 from app.config import App_Config
 from flask_restx import Api
@@ -16,7 +16,7 @@ bcrypt = Bcrypt()
 api = Api(
     version="1.0",
     title="Contact API",
-    description="API for managing contacts",
+    description="API for managing contacts 092a34a279d9446c97f1525b7803ded9",
     url_prefix="/api/v1",
 )
 
@@ -27,6 +27,7 @@ def create_app():
     """
     # Initialize Flask
     app = Flask(__name__)
+    app.config["SESSION_SQLALCHEMY"] = db
 
     # Load configuration from App_Config
     app.config.from_object(App_Config)
@@ -35,11 +36,12 @@ def create_app():
     app.url_map.strict_slashes = False
 
     # Initialize CORS
-    CORS(app)
+    CORS(app, supports_credentials=True)
 
     # Initialize Flask extensions
     db.init_app(app)
     sess.init_app(app)
+    SqlAlchemySessionInterface(app, db, "session", "_sess")
     migrate.init_app(app, db)
     api.init_app(app)
     bcrypt.init_app(app)
