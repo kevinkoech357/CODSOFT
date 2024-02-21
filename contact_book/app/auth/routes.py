80a548b3ba03b6667d9463a78a87e581156dda7b
Kevin Koech <kevinkoech@proton.me>
2024-02-21 14:08:21 +0300

    refactor auth routes to return detailed messages with status field

--- contact_book/app/auth/routes.py
+++ contact_book/app/auth/routes.py
@@ -1,14 +1,16 @@
 from flask import request, session
-from flask_restx import Resource, Namespace
+from flask_restx import Resource, Namespace, abort
 from app.models.user import User
 from app import db
 from app.marshall import registration_model, login_model
+from email_validator import validate_email, EmailNotValidError
 
 # Create a namespace for authentication routes
-auth_ns = Namespace("auth", description="Authentication operations")
+auth_ns = Namespace("auth", description="Authentication operations", validate=True)
 
 
 @auth_ns.route("/register")
+@auth_ns.doc(params={"username": "Username", "email": "Email", "password": "Password"})
 class UserRegistration(Resource):
     @auth_ns.expect(registration_model)
     def post(self):
@@ -19,33 +21,35 @@ class UserRegistration(Resource):
         password = data["password"]
 
         # Validate email
-        # try:
-        #    v = validate_email(email)
-        #   email = v["email"]
-        # except EmailNotValidError:
-        #   return {"status": "error", "message": "Invalid email address"}, 400
+        try:
+            v = validate_email(email)
+            email = v["email"]
+        except EmailNotValidError:
+            return {"status": "error", "message": "Invalid email address"}, 400
 
         # Check if username is already registered
         existing_username = User.query.filter_by(username=username).first()
         if existing_username:
-            return {"status": "error", "message": "Username already taken!"}, 400
+            abort(400, "Username already taken", status="error")
 
         # Check if email is already registered
         existing_email = User.query.filter_by(email=email).first()
         if existing_email:
-            return {"status": "error", "message": "Invalid email address"}, 400
+            abort(400, "Email address already taken.")
 
         # Create a new user
         new_user = User(username=username, email=email, password=password)
         db.session.add(new_user)
         db.session.commit()
 
-        # Placeholder for to_dict function, replace with your actual implementation
+        # Json user details
         user_details = {
             "user_id": new_user.id,
             "username": new_user.username,
             "email": new_user.email,
         }
+
+        session["user"] = {"id": new_user.id}
 
         return user_details, 200
 
@@ -61,15 +65,17 @@ class UserLogin(Resource):
 
         # Find the user by username or email
         user = User.query.filter(
-            (User.username == username_or_email) | (User.email == username_or_email)
+            (User.username == str(username_or_email))
+            | (User.email == str(username_or_email))
         ).first()
 
         if user and user.check_password(password):
             # Set the user session
-            session["user_id"] = user.id
-            return {"success": "Login successful"}, 200
+            user = session.get("user")
 
-        return {"error": "Invalid username or password"}, 401
+            return {"status": "success", "message": "Login successful"}, 200
+
+        return {"status": "error", "message": "Invalid username or password"}, 401
 
 
 @auth_ns.route("/logout")
@@ -77,5 +83,5 @@ class UserLogout(Resource):
     def post(self):
         """User logout endpoint."""
         # Clear the user session
-        session.clear()
-        return {"success": "Logout successful"}, 200
+        session.pop("user_id", None)
+        return {"status": "success", "message": "Logout successful"}, 200
